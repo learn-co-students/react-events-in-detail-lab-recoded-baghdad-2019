00b605f0ba9c013f1e7cb7f2b9ab4fb3bb79f94c
Luke Ghenco <lukeghenco@gmail.com>
2017-06-05 17:03:45 -0400

    Add code comment in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// These must be defined for code to work in browser
 import CoordinatesButton from './components/CoordinatesButton';
 import DelayedButton from './components/DelayedButton';
 
